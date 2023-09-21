bef3d142a201804d7c6a6c0602b4367efc90c2d3
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-21 00:34:14 -0600

    program complete.

--- 27_Robotic_Interface/Program.cs
+++ 27_Robotic_Interface/Program.cs
@@ -1,10 +1,105 @@
-﻿namespace _27_Robotic_Interface
+﻿namespace _27_Robotic_Interface;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Robot robot = new Robot();
+
+        for (int i = 0; i < robot.Commands.Length; i++)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write($"Give command #{i + 1} (on/off/north/south/east/west): ");
+            IRobotCommand newCommand = Console.ReadLine().ToLower() switch
+            {
+                "on" => new OnCommand(),
+                "off" => new OffCommand(),
+                "north" => new NorthCommand(),
+                "south" => new SouthCommand(),
+                "west" => new WestCommand(),
+                "east" => new EastCommand()
+            };
+
+            robot.Commands[i] = newCommand;
+        }
+
+        robot.Run();
+
+        Console.ReadLine();
+    }
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = true;
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot) => robot.IsPowered = false;
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y++;
+        }
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.Y--;
+        }
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X--;
+        }
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+        {
+            robot.X++;
         }
     }
 }
