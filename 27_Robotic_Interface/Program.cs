b2d6c3015a384cfc9e97d578e04c5e42606a3248
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-21 00:29:56 -0600

    Add project files.

---
+++ 27_Robotic_Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _27_Robotic_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
